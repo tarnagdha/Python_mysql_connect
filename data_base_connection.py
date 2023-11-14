958b5d760a0b83d30d801075ae6daa5c2d068f38
tarnagdha <tarnagdha@gmail.com>
2023-11-14 21:48:52 +0000

    connection established and query test passed

--- data_base_connection.py
+++ data_base_connection.py
@@ -1,7 +1,21 @@
-import mysql.connector
-
+import mysql.connector as mc
+from mysql.connector import Error
 from config import connection_parameters
 print(connection_parameters)
 search="'%Faible%'"
 request="SELECT * FROM utilisateur WHERE income_group LIKE " + search
 print(request)
+try :
+    connection=mc.connect(**connection_parameters)
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute(request)
+        results = cursor.fetchall()
+        print(results)
+        cursor.close()
+        connection.close()
+
+except Error as e :
+    print("la connection a échoué", e)  
+
+
